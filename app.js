2b399be18f3512d76f2205fbec451044204e19d1
hmshujaatali <81732578+hmshujaatali@users.noreply.github.com>
2021-08-18 15:13:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+//                         QUESTION#01
+// var number=+prompt("Enter a Number")
+// if(number>=0){
+//     var r=Math.round(number)
+// var c=Math.ceil(number)
+// var f=Math.floor(number)
+//     document.write("number: ",number,"<br/>",
+//     "round off vale: ",r,"<br/>",
+//     "floor value: ",f,"<br/>",
+//     "ceil value: ",c,"<br/>")
+// }
+//                         QUESTION#02
+// var number=+prompt("Enter a Number")
+// if(number<0){
+//     var r=Math.round(number)
+// var c=Math.ceil(number)
+// var f=Math.floor(number)
+//     document.write("number: ",number,"<br/>",
+//     "round off vale: ",r,"<br/>",
+//     "floor value: ",f,"<br/>",
+//     "ceil value: ",c,"<br/>")
+// }
+//                         QUESTION#03
+// var number=+prompt("Enter a Number")
+// var a=Math.abs(number)
+// document.write("Absolute value of: ",number," ",a)
+//                         QUESTION#04
+// var randomNumber=Math.random()*6
+// document.write( Math.round(randomNumber))
+//                         QUESTION#05
+// var num = Math.random() * 2
+// var a = Math.round(num)
+// if (a == 1) {
+//     document.write(a, "random coin value: Heads")
+// }
+// if (a == 2) {
+//     document.write(a, "random coin value: Tails")
+// }
+//                         QUESTION#06
+// var num=Math.random()*100
+// a=Math.round(num)
+// document.write("random number between 1 and 100: ",a)
+//                         QUESTION#07
+// var userWeight=+prompt("Enter your weight")
+// var a=Math.round(userWeight)
+// document.write("Your weight is: ", userWeight,"Kilograms")
+//                         QUESTION#08
+// var secretNumber=5
+// var userInput=+prompt("Enter a number from 1-10")
+// if(secretNumber==userInput){
+//     document.write("Congratulations! You have entered a right number")
+// }else{
+//     document.write("Sorry! You have entered a wrong number")
+// }
